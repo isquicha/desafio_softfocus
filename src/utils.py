4e13a037fb33f745a55763ec2aa37946dd170cba
Andre Vicente <isquicha@gmail.com>
2021-04-15 02:25:52 -0300

    feat: Add utils

---
+++ src/utils.py
@@ -0,0 +1,102 @@
+from time import asctime, gmtime
+
+from flask import request
+
+http_status_codes = {
+    "100": "Continue",
+    "101": "Switching Protocols",
+    "102": "Processing",
+    "200": "OK",
+    "201": "Created",
+    "202": "Accepted",
+    "203": "Non-authoritative Information",
+    "204": "No Content",
+    "205": "Reset Content",
+    "206": "Partial Content",
+    "207": "Multi-Status",
+    "208": "Already Reported",
+    "226": "IM Used",
+    "300": "Multiple Choices",
+    "301": "Moved Permanently",
+    "302": "Found",
+    "303": "See Other",
+    "304": "Not Modified",
+    "305": "Use Proxy",
+    "307": "Temporary Redirect",
+    "308": "Permanent Redirect",
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "402": "Payment Required",
+    "403": "Forbidden",
+    "404": "Not Found",
+    "405": "Method Not Allowed",
+    "406": "Not Acceptable",
+    "407": "Proxy Authentication Required",
+    "408": "Request Timeout",
+    "409": "Conflict",
+    "410": "Gone",
+    "411": "Length Required",
+    "412": "Precondition Failed",
+    "413": "Payload Too Large",
+    "414": "Request-URI Too Long",
+    "415": "Unsupported Media Type",
+    "416": "Requested Range Not Satisfiable",
+    "417": "Expectation Failed",
+    "418": "I'm a teapot",
+    "421": "Misdirected Request",
+    "422": "Unprocessable Entity",
+    "423": "Locked",
+    "424": "Failed Dependency",
+    "426": "Upgrade Required",
+    "428": "Precondition Required",
+    "429": "Too Many Requests",
+    "431": "Request Header Fields Too Large",
+    "444": "Connection Closed Without Response",
+    "451": "Unavailable For Legal Reasons",
+    "499": "Client Closed Request",
+    "500": "Internal Server Error",
+    "501": "Not Implemented",
+    "502": "Bad Gateway",
+    "503": "Service Unavailable",
+    "504": "Gateway Timeout",
+    "505": "HTTP Version Not Supported",
+    "506": "Variant Also Negotiates",
+    "507": "Insufficient Storage",
+    "508": "Loop Detected",
+    "510": "Not Extended",
+    "511": "Network Authentication Required",
+    "599": "Network Connect Timeout Error",
+}
+
+
+def json_response(
+    status_code: str = "200",
+    message: str = None,
+    path: str = request.path,
+    method: str = request.method,
+    payload: dict = None,
+) -> dict:
+    try:
+        int(status_code)
+    except ValueError:
+        status_code = 500
+        message = "Server has tried to return an invalid status!"
+
+    status_code = str(status_code)
+    response = {
+        "timestamp": asctime(gmtime()),
+        "status": status_code,
+        "method": method,
+        "path": path,
+    }
+
+    status_message = http_status_codes.get(status_code)
+    if status_message:
+        response["status_message"] = status_message
+
+    if message:
+        response["message"] = message
+    if payload:
+        response["payload"] = payload
+
+    return response, status_code
